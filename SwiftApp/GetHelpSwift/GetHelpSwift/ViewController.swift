4f4de59d86d8e9a56e99328159a302c8b77b6a4c
Xinhe <maria.shen91@gmail.com>
2015-09-21 13:32:26 -0400

    Swift Setup

---
+++ SwiftApp/GetHelpSwift/GetHelpSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetHelpSwift
+//
+//  Created by X on 9/21/15.
+//  Copyright © 2015 ITHelp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
