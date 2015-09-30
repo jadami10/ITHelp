8f75fd3b59476df8478e8f548eaa39e1b681482c
jadami10 <JA10@dhcp-ccc-3012.eduroam.cornell.edu>
2015-09-30 16:59:22 -0400

    Improves tests
    1) Checks messaging works

--- ITWebApp/main/tests.py
+++ ITWebApp/main/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase, Client
-import unittest
+import json
 
 # Create your tests here.
 
 class SimpleTest(TestCase):
+
     def setUp(self):
 
         self.client = Client()
@@ -36,22 +37,38 @@ class SimpleTest(TestCase):
 
     def test_post_bad_messages(self):
         # Issue a POST request.
-        response = self.client.post('/home/messaging/', {'text': 'hi', 'sender': 'johan'})
+        response = self.client.post('/home/messaging/', {'text': 'hi', 'senders': 'johan'})
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 400)
 
         # Issue a POST request.
-        response = self.client.post('/home/messaging/', {'text': 'hi!', 'sender': 'maria'})
+        response = self.client.post('/home/messaging/', {'text': 'hi!', 'senders': 'maria'})
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 400)
 
     def test_post_messages(self):
+
         # Issue a POST request.
-        response = self.client.post('/home/messaging/', content_type='application/json', data={'sender': 'hi', 'text': 'johan'})
+        my_data = json.dumps({'text': 'hi', 'sender': 'johan'})
+        response = self.client.post('/home/messaging/', my_data,  content_type='application/json')
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 201)
+
+        # Issue a POST request.
+        my_data = json.dumps({'text': 'hi to you', 'sender': 'maria'})
+        response = self.client.post('/home/messaging/', my_data,  content_type='application/json')
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 201)
+
+    def test_get_created_messages(self):
+
+        # Issue a GET request.
+        response = self.client.get('/home/messaging/')
+
         # Check that the response is 200 OK.
         self.assertEqual(response.status_code, 200)
 
-        # Issue a POST request.
-        response = self.client.post('/home/messaging/', content_type='application/json', data={'text': 'hi to you', 'sender': 'maria'})
-        # Check that the response is 200 OK.
-        self.assertEqual(response.status_code, 200)
+
+        # Check you get back 2 messages
+        self.assertEqual(len(response.content), 2)
+
