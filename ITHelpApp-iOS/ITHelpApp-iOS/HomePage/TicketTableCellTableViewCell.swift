ce1fe73fac26afa12438a372553ad12c66c81d30
jadami10 <johan.adami@gmail.com>
2015-11-15 22:26:29 -0500

    makes some ui fixes
    1) bug: race condition between delete and refresh
    2) adds triangles
    3) adds fake tags

--- ITHelpApp-iOS/ITHelpApp-iOS/HomePage/TicketTableCellTableViewCell.swift
+++ ITHelpApp-iOS/ITHelpApp-iOS/HomePage/TicketTableCellTableViewCell.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class TicketTableCellTableViewCell: UITableViewCell {
+class TicketTableCellTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var ticketTitleField: UITextView!
     @IBOutlet weak var ticketStatusLabel: UILabel!
     @IBOutlet weak var ticketDateLabel: UILabel!
     @IBOutlet weak var ticketTriangleImage: UIImageView!
+    @IBOutlet weak var tagViewCollection: UICollectionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        tagViewCollection.delegate = self
+        tagViewCollection.dataSource = self
         // Initialization code
         //self.portaitImageView.layer.borderColor = UIConstants.mainGreenColor.CGColor
         //self.portaitImageView.layer.borderWidth = 1.0
@@ -28,6 +31,40 @@ class TicketTableCellTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return TagManager.sharedInstance.getNumTags()
+    }
     
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TagCell", forIndexPath: indexPath) as! RequestTagCell
+        if indexPath.section == 0 {
+            let tag = TagManager.sharedInstance.getTag(indexPath)
+            cell.tagVal.text = tag.tagName
+            cell.backgroundColor = tag.tagColor
+            //            cell.tagVal.sizeToFit()
+        } else {
+            cell.backgroundColor = UIColor.whiteColor()
+        }
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let myHeight : CGFloat = 20
+        var myWidth:CGFloat
+        
+        let tagNum = TagManager.sharedInstance.getNumTags()
+        if tagNum > 0 {
+            myWidth = collectionView.frame.width/CGFloat(tagNum) - 5
+        } else {
+            myWidth = 0
+        }
+        
+        return CGSizeMake(myWidth, myHeight)
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
 
 }
